34910c984f92be94cb76909b368c4403fd769a0e
Guanqi Liu <zczl438@ucl.ac.uk>
2024-11-18 22:59:43 +0000

    Q1

--- ps3/data/_sample_split.py
+++ ps3/data/_sample_split.py
@@ -1,25 +1,50 @@
 import hashlib
+import pandas as pd
 
-import numpy as np
-
-# TODO: Write a function which creates a sample split based in some id_column and training_frac.
-# Optional: If the dtype of id_column is a string, we can use hashlib to get an integer representation.
 def create_sample_split(df, id_column, training_frac=0.8):
-    """Create sample split based on ID column.
+    """Create sample split with exact training and testing proportions.
 
     Parameters
     ----------
     df : pd.DataFrame
-        Training data
+        Input data.
     id_column : str
-        Name of ID column
+        Name of the ID column.
     training_frac : float, optional
-        Fraction to use for training, by default 0.9
+        Fraction of data to assign to the training set (default is 0.8).
 
     Returns
     -------
     pd.DataFrame
-        Training data with sample column containing train/test split based on IDs.
+        DataFrame with a new 'sample' column containing 'train' or 'test'.
     """
+    # Helper function to generate a stable hash value
+    def id_to_hash(value):
+        hashed_value = int(hashlib.md5(str(value).encode('utf-8')).hexdigest(), 16)
+        return hashed_value
+
+    # Generate a stable hash value for each ID
+    df['hash'] = df[id_column].apply(id_to_hash)
+
+    # Sort the data by the hash value
+    df = df.sort_values(by='hash').reset_index(drop=True)
+
+    # Calculate the split index based on training_frac
+    split_index = int(len(df) * training_frac)
+
+    # Assign 'train' or 'test' based on the index
+    df['sample'] = ['train' if i < split_index else 'test' for i in range(len(df))]
+
+    # Drop the intermediate hash column
+    #df.drop(columns=['hash'], inplace=True)
 
     return df
+
+# Example usage
+data = {
+    'id': ['A', 'B', 'C', 'D', 'E'],
+    'feature': [1, 2, 3, 4, 5]
+}
+df = pd.DataFrame(data)
+result = create_sample_split(df, id_column='id', training_frac=0.8)
+print(result)
